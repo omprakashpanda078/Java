243fa8e20d2e8381c0ed55feb0f407871c8865e8
Omprakash panda <ommprakashp78@gmail.com>
2026-02-05 19:23:17 +0530

    patterns in java done !

---
+++ Patterns.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int q=sc.nextInt();
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
