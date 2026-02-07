cad94654a0daf187134bfe2f42cd1de780d79d4e
Omprakash panda <ommprakashp78@gmail.com>
2026-02-07 19:46:31 +0530

    Roted half pyramid done !

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int n = 4;
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n -i; j++) {
+                System.out.print(" ");
+            }
+            // inner loop for star
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
