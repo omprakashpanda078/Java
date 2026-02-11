251feb5690ffb9a8a1fc722d48d850ef53ff6f1a
omprakash panda <ommprakashp78@gmail.com>
2026-02-11 11:43:55 +0530

    multiply two number

--- function.java
+++ function.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class function {
-    public static int operateSum(int a, int b) {
-        int sum = a + b;
-        return sum;
+    public static int multiply(int a, int b) {
+        int multiply = a * b;
+        return a * b;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = operateSum(a, b);
-        System.out.println("the sum of a and b is " + sum);
+        int product = multiply(a, b);
+        System.out.println("the multiplication of a and b is:"+product);
     }
 }
