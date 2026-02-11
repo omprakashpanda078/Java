b674c3ccc6f16e7fd28bf32ff4f0920a86d16ec5
omprakash panda <ommprakashp78@gmail.com>
2026-02-11 11:24:12 +0530

    add the num by function

--- function.java
+++ function.java
@@ -1,14 +1,16 @@
 import java.util.*;
 
 public class function {
-    public static void printMyName(String name) {
-        System.out.println(name);
-        return;
+    public static int operateSum(int a, int b) {
+        int sum = a + b;
+        return sum;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        printMyName(name);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = operateSum(a, b);
+        System.out.println("the sum of a and b is " + sum);
     }
 }
