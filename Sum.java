753fc20d7ff00ba4d8ee7669c5e354d338350cb4
omprakash panda <ommprakashp78@gmail.com>
2026-02-16 20:20:49 +0530

    another question done !

--- Sum.java
+++ Sum.java
@@ -1,20 +1,14 @@
 import java.util.*;
 
 public class Sum {
-
-    static void printOddSum(int n) {
-        int sum = 0;
-
-        for (int i = 1; i <= n; i += 2) {
-            sum += i;
-        }
-
-        System.out.println("Sum of odd numbers from 1 to " + n + " is: " + sum);
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        printOddSum(n);
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        if(n>m){
+            System.out.println("n is greater"+n);
+        }else if(n<m){
+            System.out.println("m is greater"+m);
+        }
     }
 }
