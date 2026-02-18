1085cd10f1b740d5111ff65062717e9b1a491b45
omprakash panda <ommprakashp78@gmail.com>
2026-02-18 21:12:09 +0530

    infinite loop !

---
+++ Om1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Om1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        do {
+            System.out.println("infinite");
+        } while (n>=0);
+
+    }
+}
