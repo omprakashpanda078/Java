de7ef9063a4c8e3524cdfa84b2361aa8d8d79439
omprakash panda <ommprakashp78@gmail.com>
2026-03-02 13:33:59 +0530

    Array in java

---
+++ Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String args[]) {
+        int[]marks=new int[4];
+         marks[0]=97;
+         marks[1]=98;
+         marks[2]=99;
+         marks[3]=100;
+        for(int i=0;i<4;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
